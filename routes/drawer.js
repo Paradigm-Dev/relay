d3609bb27f7094471d685f3a23a74c48c0a803f8
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-04-15 07:06:18 -0400

    Added support for Write files

--- routes/drawer.js
+++ routes/drawer.js
@@ -7,23 +7,11 @@ const moment = require('moment')
 
 const UserModel = require('../models/User.js')
 
-// router.get('/list/:username', (req, res) => {
-//   UserModel.findOne({ username: req.params.username }, (error, data) => {
-//     if (!error) res.json(data.files)
-//   })
-  // var dir = path.join(__dirname + '/../drawer/' + req.params.username)
-  // if (fs.existsSync(dir)) {
-  //   fs.readdir(dir, (error, filenames) => {
-  //     if (error) {
-  //       console.error(error)
-  //     } else {
-  //       res.json(filenames)
-  //     }
-  //   })
-  // } else {
-  //   fs.mkdirSync(dir)
-  // }
-// })
+router.get('/:username/list', (req, res) => {
+  UserModel.findOne({ username: req.params.username }, (error, data) => {
+    if (!error) res.json(data.files)
+  })
+})
 
 router.get('/:username/download/:id', async (req, res) => {
   var User = await UserModel.findOne({ username: req.params.username })
@@ -31,14 +19,22 @@ router.get('/:username/download/:id', async (req, res) => {
   res.download(path.join(__dirname + '/../drawer/' + req.params.username + '/' + File.path))
 })
 
+router.get('/:username/get/:id', async (req, res) => {
+  var User = await UserModel.findOne({ username: req.params.username })
+  var File = await User.files.id(req.params.id)
+  res.sendFile(path.join(__dirname + '/../drawer/' + req.params.username + '/' + File.path))
+})
+
 router.post('/:username/rename/:id', async (req, res) => {
   var User = await UserModel.findOne({ username: req.params.username })
   var File = await User.files.id(req.params.id)
-  fs.rename(__dirname + '/../drawer/' + req.params.username + '/' + File.path, __dirname + '/../drawer/' + req.params.username + '/' + req.body.name + '.' +File.path.slice(File.path.lastIndexOf('.') + 1), (err) => {
+  var newPath
+  if (File.type == 'workshop/write') newPath = req.body.name + '.write.json'
+  else newPath = req.body.name + '.' + File.path.slice(File.path.lastIndexOf('.') + 1)
+  fs.rename(__dirname + '/../drawer/' + req.params.username + '/' + File.path, __dirname + '/../drawer/' + req.params.username + '/' + newPath, (err) => {
     if (err) throw err;
   })
-  var oldPath = File.path
-  File.path = req.body.name + '.' + oldPath.slice(oldPath.lastIndexOf('.') + 1)
+  File.path = newPath
   File.name = req.body.name
   await User.save()
   res.end()
@@ -55,6 +51,33 @@ router.delete('/:username/delete/:id', async (req, res) => {
   })
 })
 
+router.post('/:username/upload/write', (req, res) => {
+  console.log('Saving new Write file...')
+  const pathway = __dirname + `/../drawer/${req.params.username}/${req.body.title}.write.json`
+  if (fs.existsSync(pathway)) fs.unlinkSync(pathway)
+  fs.writeFile(pathway, JSON.stringify(req.body), error => {
+    if (error) console.error(error)
+    console.log('The file has been saved!')
+    fs.stat(pathway, async (error, stats) => {
+      if (error) console.error(error)
+      var User = await UserModel.findOne({ username: req.params.username })
+      let exists = false
+      User.files.forEach(file => {
+        if (file.name == req.body.title) exists = true
+      })
+      if (!exists) User.files.push({
+        name: req.body.title,
+        type: 'workshop/write',
+        size: stats.size + 'B',
+        date: moment().format('MM/DD/YYYY [at] HH:MM a'),
+        path: `${req.body.title}.write.json`
+      })
+      await User.save()
+      res.json(req.body)
+    })
+  })
+})
+
 router.post('/:username/upload', (req, res) => {
   var form = new formidable.IncomingForm()
 
@@ -64,6 +87,10 @@ router.post('/:username/upload', (req, res) => {
     var dir = path.join(__dirname + '/../drawer/' + req.params.username)
     if (!fs.existsSync(dir)) fs.mkdirSync(dir)
     file.path = __dirname + '/../drawer/' + req.params.username + '/' + file.name
+    if (fs.existsSync(file.path)) {
+      form.pause()
+      res.end()
+    }
   })
 
   form.on('file', async (name, file) => {
