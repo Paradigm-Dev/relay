5c710377ccb44b4590f1c80ef68c6c61cccbe700
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-05-14 14:37:55 -0400

    Drawer uses UID insted of username

--- routes/drawer.js
+++ routes/drawer.js
@@ -7,31 +7,31 @@ const moment = require('moment')
 
 const UserModel = require('../models/User.js')
 
-router.get('/:username/list', (req, res) => {
-  UserModel.findOne({ username: req.params.username }, (error, data) => {
+router.get('/:uid/list', (req, res) => {
+  UserModel.findOne({ _id: req.params.uid }, (error, data) => {
     if (!error) res.json(data.files)
   })
 })
 
-router.get('/:username/download/:id', async (req, res) => {
-  var User = await UserModel.findOne({ username: req.params.username })
+router.get('/:uid/download/:id', async (req, res) => {
+  var User = await UserModel.findOne({ _id: req.params.uid })
   var File = await User.files.id(req.params.id)
-  res.download(path.join(__dirname + '/../drawer/' + req.params.username + '/' + File.path))
+  res.download(path.join(__dirname + '/../drawer/' + req.params.uid + '/' + File.path))
 })
 
-router.get('/:username/get/:id', async (req, res) => {
-  var User = await UserModel.findOne({ username: req.params.username })
+router.get('/:uid/get/:id', async (req, res) => {
+  var User = await UserModel.findOne({ _id: req.params.uid })
   var File = await User.files.id(req.params.id)
-  res.sendFile(path.join(__dirname + '/../drawer/' + req.params.username + '/' + File.path))
+  res.sendFile(path.join(__dirname + '/../drawer/' + req.params.uid + '/' + File.path))
 })
 
-router.post('/:username/rename/:id', async (req, res) => {
-  var User = await UserModel.findOne({ username: req.params.username })
+router.post('/:uid/rename/:id', async (req, res) => {
+  var User = await UserModel.findOne({ _id: req.params.uid })
   var File = await User.files.id(req.params.id)
   var newPath
   if (File.type == 'workshop/write') newPath = req.body.name + '.write.json'
   else newPath = req.body.name + '.' + File.path.slice(File.path.lastIndexOf('.') + 1)
-  fs.rename(__dirname + '/../drawer/' + req.params.username + '/' + File.path, __dirname + '/../drawer/' + req.params.username + '/' + newPath, (err) => {
+  fs.rename(__dirname + '/../drawer/' + req.params.uid + '/' + File.path, __dirname + '/../drawer/' + req.params.uid + '/' + newPath, (err) => {
     if (err) throw err;
   })
   File.path = newPath
@@ -40,10 +40,10 @@ router.post('/:username/rename/:id', async (req, res) => {
   res.end()
 })
 
-router.delete('/:username/delete/:id', async (req, res) => {
-  var User = await UserModel.findOne({ username: req.params.username })
+router.delete('/:uid/delete/:id', async (req, res) => {
+  var User = await UserModel.findOne({ _id: req.params.uid })
   var File = await User.files.id(req.params.id)  
-  fs.unlink(path.join(__dirname + '/../drawer/' + req.params.username + '/' + File.path), async error => {
+  fs.unlink(path.join(__dirname + '/../drawer/' + req.params.uid + '/' + File.path), async error => {
     await File.remove()
     await User.save()
     res.end()
@@ -51,16 +51,14 @@ router.delete('/:username/delete/:id', async (req, res) => {
   })
 })
 
-router.post('/:username/upload/write', (req, res) => {
-  console.log('Saving new Write file...')
-  const pathway = __dirname + `/../drawer/${req.params.username}/${req.body.title}.write.json`
+router.post('/:uid/upload/write', (req, res) => {
+  const pathway = __dirname + `/../drawer/${req.params._uid}/${req.body.title}.write.json`
   if (fs.existsSync(pathway)) fs.unlinkSync(pathway)
   fs.writeFile(pathway, JSON.stringify(req.body), error => {
     if (error) console.error(error)
-    console.log('The file has been saved!')
     fs.stat(pathway, async (error, stats) => {
       if (error) console.error(error)
-      var User = await UserModel.findOne({ username: req.params.username })
+      var User = await UserModel.findOne({ _id: req.params.uid })
       let exists = false
       User.files.forEach(file => {
         if (file.name == req.body.title) exists = true
@@ -78,15 +76,41 @@ router.post('/:username/upload/write', (req, res) => {
   })
 })
 
-router.post('/:username/upload', (req, res) => {
+router.post('/:uid/upload/sales', (req, res) => {
+  const pathway = __dirname + `/../drawer/${req.params.uid}/${req.body.title}.sales.json`
+  if (fs.existsSync(pathway)) fs.unlinkSync(pathway)
+  fs.writeFile(pathway, JSON.stringify(req.body), error => {
+    if (error) console.error(error)
+    fs.stat(pathway, async (error, stats) => {
+      if (error) console.error(error)
+      var User = await UserModel.findOne({ _id: req.params.uid })
+      let exists = false
+      User.files.forEach(file => {
+        if (file.name == req.body.title) exists = true
+      })
+      if (!exists) User.files.push({
+        name: req.body.title,
+        type: 'workshop/sales',
+        size: stats.size + 'B',
+        date: moment().format('MM/DD/YYYY [at] HH:MM a'),
+        path: `${req.body.title}.sales.json`
+      })
+      await User.save()
+      res.json(req.body)
+      
+    })
+  })
+})
+
+router.post('/:uid/upload', (req, res) => {
   var form = new formidable.IncomingForm()
 
   form.parse(req)
 
   form.on('fileBegin', (name, file) => {
-    var dir = path.join(__dirname + '/../drawer/' + req.params.username)
+    var dir = path.join(__dirname + '/../drawer/' + req.params.uid)
     if (!fs.existsSync(dir)) fs.mkdirSync(dir)
-    file.path = __dirname + '/../drawer/' + req.params.username + '/' + file.name
+    file.path = __dirname + '/../drawer/' + req.params.uid + '/' + file.name
     if (fs.existsSync(file.path)) {
       form.pause()
       res.end()
@@ -94,7 +118,7 @@ router.post('/:username/upload', (req, res) => {
   })
 
   form.on('file', async (name, file) => {
-    var User = await UserModel.findOne({ username: req.params.username })
+    var User = await UserModel.findOne({ _id: req.params.uid })
     User.files.push({
       name: file.name.slice(0, file.name.lastIndexOf('.')),
       type: file.type,
