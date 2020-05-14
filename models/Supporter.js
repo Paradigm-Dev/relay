91e41931da6b8b7d0a78b79c7efa0e4037a7ea23
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-05-14 14:37:40 -0400

    Campaign

---
+++ models/Supporter.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const SupporterSchema = new mongoose.Schema({
+  f_name: String,
+  l_name: String,
+  email: String,
+  phone: String,
+  notify: Boolean,
+  wishes: String
+})
+
+const SupporterModel = mongoose.model('supporter', SupporterSchema)
+
+module.exports = SupporterModel
