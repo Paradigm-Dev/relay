83bc4ba8d6bf5042be1a0fc88da5e9dd56d669e0
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-06-30 08:32:12 -0400

    Book and Movie uploader

--- routes/media.js
+++ routes/media.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const formidable = require('formidable')
+const Jimp = require('jimp')
+const fs = require('fs')
+
 const router = express.Router()
 
 const BookModel = require('../models/Book.js')
@@ -112,5 +117,65 @@ router.get('/music/read/:title', (req, res) => {
   })
 })
 
+
+// Upload
+router.post('/create/data', async (req, res) => {
+  console.log(req.body)
+  let _id
+  switch (req.body.type) {
+    case 'book':
+      _id = new mongoose.Types.ObjectId()
+      await BookModel.create({
+        _id,
+        author: req.body.author,
+        live: true,
+        cover: `https://www.theparadigmdev.com/relay/books/img/${_id.toString()}.jpg`,
+        link: `https://www.theparadigmdev.com/relay/books/${_id.toString()}.pdf`,
+        summary: req.body.summary,
+        title: req.body.title
+      }, (error, book) => res.json({ _id: book._id }))
+      break
+    case 'movie':
+      _id = new mongoose.Types.ObjectId()
+      await MovieModel.create({
+        _id,
+        genre: req.body.genre,
+        live: true,
+        cover: `https://www.theparadigmdev.com/relay/movies/img/${_id.toString()}.jpg`,
+        link: `https://www.theparadigmdev.com/relay/movies/${_id.toString()}.pdf`,
+        summary: req.body.summary,
+        title: req.body.title
+      }, (error, movie) => res.json({ _id: movie._id }))
+      console.log(_id)
+      break
+  }
+})
+
+router.post('/create/:id/files/:type', async (req, res) => {
+  var Item
+  switch (req.params.type) {
+    case 'book': Item = await BookModel.findOne({ _id: req.params.id }); break;
+    case 'movie': Item = await MovieModel.findOne({ _id: req.params.id }); break;
+    // case 'music': Item = await MusicModel.findOne({ _id: req.params.id })
+  }
+
+  const form = formidable({ multiples: false, uploadDir: __dirname + `/../files/${req.params.type == 'music' ? 'music' : req.params.type + 's'}/`, keepExtensions: true })
+
+  await form.parse(req, async (err, fields, files) => {
+    if (err) {
+      next(err)
+      return
+    }
+
+    console.log(files)
+    console.log(Item)
+
+    await Jimp.read(files.cover.path).then(img => {
+      return img.write(__dirname + `/../files/${req.params.type == 'music' ? 'music' : req.params.type + 's'}/img/${Item._id}.jpg`)
+    }).catch(error => console.error(error))
+    await fs.renameSync(files.file.path, __dirname + `/../files/${req.params.type == 'music' ? 'music' : req.params.type + 's'}/${Item._id}.${files.file.name.slice(files.file.name.lastIndexOf('.'))}`)
+    res.json(Item)
+  })
+})
 
 module.exports = router
